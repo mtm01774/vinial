eff9afbc999491f8e40ebfb0a4ac3eab08f61e7e
mtm01774 <luisdanielcunha@gmail.com>
2025-02-01 22:04:20 +0000

    feat: Add i18n support and middleware

--- config/next.config.js
+++ config/next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  i18n: {
+    locales: ['pt', 'en'],
+    defaultLocale: 'pt',
+    localeDetection: true,
+  },
 }
 
 module.exports = nextConfig 
